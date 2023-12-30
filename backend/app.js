642bf96caa446ed158652fc66d49444c9df9950e
Marina Kiryanova <martinkazz@yandex.ru>
2023-12-30 20:52:45 +0200

    PW-15 fix localhost

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const { validateCreateUser, validateLoginUser } = require('./middlewares/validat
 
 const {
   PORT = 3000,
-  MONGO_URL = 'mongodb://localhost:27017/mestodb',
+  MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb',
 } = process.env;
 
 mongoose.connect(`${MONGO_URL}`, {
